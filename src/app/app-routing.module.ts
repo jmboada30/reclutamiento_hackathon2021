cec184f4d085ded0d927a76c0a8ee29df287b129
Joel Boada Srv Worknet <jmboada30@gmail.com>
2021-09-25 18:04:06 -0500

    add module layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () =>
+      import('./layouts/layouts.module').then((m) => m.LayoutsModule),
+  },
+  {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
