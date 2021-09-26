26bba1695d713da6d0124f760580523057a0a6e0
juan <choqueguzmanj@gmail.com>
2021-09-25 20:13:02 -0400

    -createpantalla usuario bootcamps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BootcampsComponent } from './users/pages/bootcamps/bootcamps.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BootcampsComponent
   ],
   imports: [
     BrowserModule,
