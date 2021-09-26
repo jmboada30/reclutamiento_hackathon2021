5a36e6267d9be53c0b328a9cfba75af84b7a466b
juan <choqueguzmanj@gmail.com>
2021-09-25 23:33:33 -0400

    -createpantalla usuario Inscripciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BootcampsComponent } from './users/pages/bootcamps/bootcamps.component';
+import { InscripcionesComponent } from './users/pages/inscripciones/inscripciones.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BootcampsComponent
+    BootcampsComponent,
+    InscripcionesComponent
   ],
   imports: [
     BrowserModule,
